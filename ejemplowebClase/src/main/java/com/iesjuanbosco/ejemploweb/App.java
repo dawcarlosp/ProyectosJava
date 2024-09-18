dfbfcb1ca0e79213c3914f9338c6abc211c3f5ef
Carlos Pereira <dawcarlospereira@gmail.com>
2024-09-18 16:30:41 +0200

    Separación de lo que se hace en clase

---
+++ ejemplowebClase/src/main/java/com/iesjuanbosco/ejemploweb/App.java
@@ -0,0 +1,12 @@
+package com.iesjuanbosco.ejemploweb;
+
+import com.iesjuanbosco.ejemploweb.controller.ProductoController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
