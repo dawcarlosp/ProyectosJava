b90f999971b6c9a187c29b11e27c73274ef4d915
Carlos <dawcarlospereira@gmail.com>
2024-10-13 12:49:10 +0200

    Ahora solo se muestran los comentarios del producto que se esta visualizando

--- ejemplowebPersonal/src/main/java/com/iesjuanbosco/ejemploweb/controller/ProductoController.java
+++ ejemplowebPersonal/src/main/java/com/iesjuanbosco/ejemploweb/controller/ProductoController.java
@@ -106,6 +106,7 @@ public class ProductoController {
         if(producto.isPresent()){
             model.addAttribute("producto", producto.get());
             model.addAttribute("comentario" , new Comentario());
+            model.addAttribute("comentarios", this.comentarioRepository.findComentarioByProducto(producto.get()));
             return "/producto/producto-view";
         }else{
             return "redirect:/productos/";
