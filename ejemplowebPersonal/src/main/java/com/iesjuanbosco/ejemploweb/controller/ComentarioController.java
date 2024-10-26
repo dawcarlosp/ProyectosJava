31189d529fa8a157db6e775e843b6c0c6d0872f0
Carlos Pereira <dawcarlospereira@gmail.com>
2024-10-26 15:07:09 +0200

    Creado ComentaarioService, mayor separación de la lógina, aunque no una total

--- ejemplowebPersonal/src/main/java/com/iesjuanbosco/ejemploweb/controller/ComentarioController.java
+++ ejemplowebPersonal/src/main/java/com/iesjuanbosco/ejemploweb/controller/ComentarioController.java
@@ -5,6 +5,7 @@ import com.iesjuanbosco.ejemploweb.entity.Comentario;
 import com.iesjuanbosco.ejemploweb.entity.Producto;
 import com.iesjuanbosco.ejemploweb.repository.ComentarioRepository;
 import com.iesjuanbosco.ejemploweb.repository.ProductoRepository;
+import com.iesjuanbosco.ejemploweb.service.ComentarioService;
 import com.iesjuanbosco.ejemploweb.service.ProductoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,41 +25,24 @@ import java.util.Optional;
 public class ComentarioController {
     @Autowired
     private ProductoService productoService;
-
-    private ComentarioRepository comentarioRepository;
-    private ProductoRepository productoRepository;
-    public ComentarioController(ComentarioRepository comentarioRepository, ProductoRepository productoRepository) {
-        this.comentarioRepository = comentarioRepository;
-        this.productoRepository = productoRepository;
-    }
+    @Autowired
+    private ComentarioService comentarioService;
     //Comentar producto
     @PostMapping("/productos/comentarios/new/{id}")
     public String comentar(@PathVariable Long id, @Valid Comentario comentario, BindingResult bindingResult, Model model){
-        Optional<Producto> producto = this.productoRepository.findById(id);
-        if(producto.isPresent()) {
-            if (bindingResult.hasErrors()) {
-                model.addAttribute("producto",producto.get());
-                model.addAttribute("comentarios",this.comentarioRepository.findByProductoOrderByFechaDesc(producto.get()));
-                return "/producto/producto-view";
-            }
-            comentario.setProducto(producto.get());
-            comentario.setFecha(LocalDate.now());
-            this.comentarioRepository.save(comentario);
-            return "redirect:/productos/view/" + id;
-        }
-        return "redirect:/productos";
+        return this.comentarioService.comentar(id, comentario, bindingResult, model);
     }
     //Listar comentarios
     @GetMapping("/comentarios")
     public String findAll(Model model){
-        model.addAttribute("comentarios", this.comentarioRepository.findAll());
+        model.addAttribute("comentarios", this.comentarioService.findAll());
         return "/comentario/comentario-list";
     }
     //Alta comentario
     @GetMapping("/comentarios/new")
     public String newComentarioVista(Model model){
         List<Producto> productos = new ArrayList<>();
-        productos = this.productoRepository.findAll();
+        productos = this.productoService.findAll();
         model.addAttribute("productos", productos);
         model.addAttribute("comentario", new Comentario());
         return "/comentario/comentario-new";
@@ -68,22 +52,22 @@ public class ComentarioController {
         if(bindingResult.hasErrors()){
             return "/comentario/comentario-new";
         }
-        this.comentarioRepository.save(comentario);
+        this.comentarioService.save(comentario);
         return "redirect:/comentarios";
     }
     //Eliminar comentario
     @GetMapping ("/comentarios/del/{id}")
     public String eliminarComentario(@PathVariable Long id){
-        this.comentarioRepository.deleteById(id);
+        this.comentarioService.deleteById(id);
         return "redirect:/comentarios";
     }
     //editar comentario
     @GetMapping("/comentarios/edit/{id}")
     public String editComentario(Model model, @PathVariable Long id){
-        Optional<Comentario> comentario = this.comentarioRepository.findById(id);
+        Optional<Comentario> comentario = this.comentarioService.findById(id);
         if(comentario.isPresent()){
             model.addAttribute("comentario",comentario.get());
-            model.addAttribute("productos", this.productoRepository.findAll());
+            model.addAttribute("productos", this.productoService.findAll());
             return "comentario/comentario-edit";
         }else{
             return "redirect:/comentarios";
@@ -94,12 +78,12 @@ public class ComentarioController {
         if (bindingResult.hasErrors()){
             return "comentario/comentario-edit";
         }
-        this.comentarioRepository.save(comentario);
+        this.comentarioService.save(comentario);
         return "redirect:/comentarios";
     }
     @GetMapping("/comentarios/view/{id}")
     public String verComentario(@PathVariable Long id, Model model){
-        Optional<Comentario> comentario = this.comentarioRepository.findById(id);
+        Optional<Comentario> comentario = this.comentarioService.findById(id);
         if(comentario.isPresent()){
             model.addAttribute("comentario", comentario.get());
             return "/comentario/comentario-view";
---
+++ ejemplowebPersonal/src/main/java/com/iesjuanbosco/ejemploweb/service/ComentarioService.java
@@ -0,0 +1,51 @@
+package com.iesjuanbosco.ejemploweb.service;
+
+import com.iesjuanbosco.ejemploweb.entity.Comentario;
+import com.iesjuanbosco.ejemploweb.entity.Producto;
+import com.iesjuanbosco.ejemploweb.repository.ComentarioRepository;
+import com.iesjuanbosco.ejemploweb.repository.ProductoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ComentarioService {
+    @Autowired
+    private ProductoService productoService;
+    @Autowired
+    private ComentarioRepository comentarioRepository;
+    public String comentar(@PathVariable Long id, @Valid Comentario comentario, BindingResult bindingResult, Model model){
+        Optional<Producto> producto = productoService.findById(id);
+        if(producto.isPresent()) {
+            if (bindingResult.hasErrors()) {
+                model.addAttribute("producto",producto.get());
+                model.addAttribute("comentarios",this.comentarioRepository.findByProductoOrderByFechaDesc(producto.get()));
+                return "/producto/producto-view";
+            }
+            comentario.setProducto(producto.get());
+            comentario.setFecha(LocalDate.now());
+            this.comentarioRepository.save(comentario);
+            return "redirect:/productos/view/" + id;
+        }
+        return "redirect:/productos";
+    }
+    public List<Comentario> findAll(){
+        return this.comentarioRepository.findAll();
+    }
+    public void save(Comentario comentario){
+        this.comentarioRepository.save(comentario);
+    }
+    public void deleteById(@PathVariable Long id){
+        this.comentarioRepository.deleteById(id);
+    }
+    public Optional findById(Long id){
+        return this.comentarioRepository.findById(id);
+    }
+}
